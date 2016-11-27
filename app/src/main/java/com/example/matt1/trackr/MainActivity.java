4ad770e4a455cc2593b09a8c7713a8a5c985a9c4
Matthew Bell <contact@mdbell.me>
2016-11-26 22:31:55 -0500

    made ui more responsive

--- app/src/main/java/com/example/matt1/trackr/MainActivity.java
+++ app/src/main/java/com/example/matt1/trackr/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton = (Button)findViewById(R.id.login_button);
+        final Button loginButton = (Button)findViewById(R.id.login_button);
+        final TextView username = (TextView)findViewById(R.id.username);
+        final TextView password = (TextView)findViewById(R.id.password);
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                loginButton.setEnabled(username.getText().length() > 0 && password.getText().length() > 0);
+            }
+        };
+        username.addTextChangedListener(watcher);
+        password.addTextChangedListener(watcher);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra(LoginAttemptActivity.USERNAME_KEY,username.getText().toString());
         i.putExtra(LoginAttemptActivity.PASSWORD_KEY,password.getText().toString());
 
-        finish();
+        //finish();
         startActivity(i);
     }
 }
